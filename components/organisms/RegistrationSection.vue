539aecd12d1abcd216c65a08f1a05ec357f008af
fshlarf <faishal.arif218@gmail.com>
2020-08-22 12:53:38 +0700

    active packet spotify

--- components/organisms/RegistrationSection.vue
+++ components/organisms/RegistrationSection.vue
@@ -187,7 +187,7 @@ export default {
             showPacket: false,
             providers: [
                 {name: 'Netflix', active: true},
-                {name: 'Spotify', active: false},
+                {name: 'Spotify', active: true},
                 {name: 'Youtube', active: false},
                 {name: 'Steam', active: false},
                 {name: 'Joox', active: false}
