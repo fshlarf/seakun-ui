9fec2f0b0af153d22574f1932ee42b42bf737e9c
ahmadfsal <ahmadfaisal0231@gmail.com>
2020-08-31 20:02:14 +0700

    redesign testimoni section, add new image assets

--- components/organisms/TestimoniSection.vue
+++ components/organisms/TestimoniSection.vue
@@ -3,42 +3,65 @@
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h5 class="testimoni__header-h5" style="margin-bottom: 30px">Apa kata mereka yang menggunakan Seakun.id?</h5>
+                    <h5
+                        class="testimoni__header-h5"
+                        style="margin-bottom: 30px"
+                    >Apa kata mereka yang menggunakan Seakun.id?</h5>
                     <div class="testimoni__content">
                         <div class="testimoni__content-card">
-                            <h5>Geby Septia</h5>
-                            <p class="label">Pelanggan Netflix via Seakun.id</p>
+                            <figure class="testimoni__content author-thumbnail">
+                                <img src="images/girl.png" alt="image not found" />
+                            </figure>
                             <p>
-                                "Menurut aku ya, seakun.id itu bagus bgt. Segala sesuatunya lebih mudah, 
-                                dulu kalo mau masang <b>mikir2 karna untuk yg premium sangat mahal</b>. 
-                                Tp karna ada seakun.id, aku dan suamiku jadi bisa nonton film. 
-                                Dengan bayar 49 rb sudah dpt premium. Thanks seakun.id."
+                                Menurut aku ya, seakun.id itu bagus bgt. Segala sesuatunya lebih mudah,
+                                dulu kalo mau masang
+                                <b>mikir2 karna untuk yg premium sangat mahal</b>.
+                                Tp karna ada seakun.id, aku dan suamiku jadi bisa nonton film.
+                                Dengan bayar 49 rb sudah dpt premium. Thanks seakun.id.
                             </p>
+                            <div class="info">
+                                Geby Septia -
+                                <span class="designation">Netflix</span>
+                            </div>
                         </div>
                         <div class="testimoni__content-card">
-                            <h5>Agita Widianti</h5>
-                            <p class="label">Pelanggan Netflix via Seakun.id</p>
+                            <figure class="testimoni__content author-thumbnail">
+                                <img src="images/girl.png" alt="image not found" />
+                            </figure>
                             <p>
-                                "Aku beruntung banget pas liat di Instagram ada seakun.id. 
-                                <b>Ngebantu aku banget</b> yang pingin langganan Netflix paket premium 
-                                dengan harga yang ekonomis banget. Untuk layananya juga oke banget kok. 
-                                Responnya cepet banget <b>proses pembuatan akunya juga gak lama</b>. 
-                                Jadi aku langsung bisa nikmatin film2 yang aku suka melalui Netflix. 
+                                Aku beruntung banget pas liat di Instagram ada seakun.id.
+                                <b>Ngebantu aku banget</b> yang pingin langganan Netflix paket premium
+                                dengan harga yang ekonomis banget. Untuk layananya juga oke banget kok.
+                                Responnya cepet banget
+                                <b>proses pembuatan akunya juga gak lama</b>.
+                                Jadi aku langsung bisa nikmatin film2 yang aku suka melalui Netflix.
                                 Adminnya juga ramah.
-                                Terimakasih, sukses terus untuk Seakun.id."
+                                Terimakasih, sukses terus untuk Seakun.id.
                             </p>
+                            <div class="info">
+                                Agita Widianti -
+                                <span class="designation">Netflix</span>
+                            </div>
                         </div>
                         <div class="testimoni__content-card">
-                            <h5>Faishal Arif</h5>
-                            <p class="label">Pelanggan Netflix via Seakun.id</p>
+                            <figure class="testimoni__content author-thumbnail">
+                                <img src="images/boy.png" alt="image not found" />
+                            </figure>
                             <p>
-                                "Menurut saya <b>Seakun.id ngebantu banget pada masa pandemi ini</b>.
+                                Menurut saya
+                                <b>Seakun.id ngebantu banget pada masa pandemi ini</b>.
                                 Saya juga butuh hiburan karena bioskop lagi tutup. Kebetulan saya sudah lama
-                                ingin berlangganan Netflix, tapi karena <b>enggak punya Kartu Kredit</b> jadi belum bisa.
+                                ingin berlangganan Netflix, tapi karena
+                                <b>enggak punya Kartu Kredit</b> jadi belum bisa.
                                 Alhamdulillah ada Seakun.id yang ngurus semua proses berlangganan
-                                ke Netflix, jadi saya <b>gak perlu repot</b> urus payment ke Netflixnya,
-                                Mantep deh pokoknya, gak nyesel pake Seakun.id."
+                                ke Netflix, jadi saya
+                                <b>gak perlu repot</b> urus payment ke Netflixnya,
+                                Mantep deh pokoknya, gak nyesel pake Seakun.id.
                             </p>
+                            <div class="info">
+                                Ahmad Faisal -
+                                <span class="designation">Netflix</span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -48,8 +71,7 @@
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
@@ -70,7 +92,7 @@ export default {
         }
         button {
             margin: 10px;
-            padding: 6px 35px !important
+            padding: 6px 35px !important;
         }
         .label {
             margin-bottom: 20px !important;
@@ -81,7 +103,7 @@ export default {
         }
     }
     .container {
-        max-width: 1500px!important;
+        max-width: 1500px !important;
         font-weight: 500 !important;
         margin: 0 auto !important;
     }
@@ -93,27 +115,54 @@ export default {
     .row {
         padding: 0px 8px !important;
     }
+
+    .info {
+        font-weight: 700;
+        font-size: 15px;
+        color: #2a2a2a;
+        text-transform: uppercase;
+        line-height: 26px;
+        margin-bottom: 20px;
+    }
+
+    .designation {
+        font-weight: 400;
+        font-size: 14px;
+    }
+
     &__content {
         display: flex;
         justify-content: space-around;
+
+        &.author-thumbnail {
+            position: relative;
+            margin: 0 auto 35px !important;
+            img {
+                border: 2px dashed;
+                border-radius: 50%;
+                border-color: #86d0c1;
+                padding: 10px;
+            }
+        }
+
         &-card {
             width: 29rem;
             margin: 10px 6px;
-            text-align: left;
+            text-align: center;
             padding: 16px 24px;
             border: 1px solid #eeeeee;
             border-radius: 8px;
             box-shadow: 0px 0px 4px #dddddd;
             cursor: pointer;
-            transition: transform .2s;
+            transition: transform 0.2s;
             &:hover {
                 box-shadow: 0px 0px 16px #dddddd;
                 transform: scale(1.05);
             }
             p {
-                margin: 0px!important;
-                font-size: 13px;
-                font-style: italic;
+                margin: 0 auto 25px !important;
+                position: relative;
+                line-height: 1.8em;
             }
         }
     }
@@ -141,20 +190,20 @@ export default {
             }
         }
         img {
-            width: 180px!important;
+            width: 180px !important;
             margin: 16px auto;
         }
         .row {
-            display:block;
+            display: block;
         }
         h2 {
             font-size: 20px;
         }
     }
 }
-@media  only screen and (min-width: 880px) and (max-width: 1020px) {
+@media only screen and (min-width: 880px) and (max-width: 1020px) {
     .container {
-        display: contents!important;
+        display: contents !important;
     }
 }
 </style>
