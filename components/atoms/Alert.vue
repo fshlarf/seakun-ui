fa331b3c8766d6a7f1816ef24cd5256c666d9cf6
ahmadfsal <ahmadfaisal0231@gmail.com>
2020-09-05 13:03:55 +0700

    Add maintenance section

---
+++ components/atoms/Alert.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="alert alert-danger">
+        <div class="row">
+            <div class="col col-lg-1 text-center">
+                <span class="fa fa-info-circle"></span>
+            </div>
+            <div class="col">{{ message }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        message: "",
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
