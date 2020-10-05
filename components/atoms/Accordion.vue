2e48f1dbc82f702947552f044d593e8c60484c8b
ahmadf04 <ahmad.faisal@elevenia.co.id>
2020-10-05 12:12:16 +0700

    Refactor component, add scheme of price page

--- components/atoms/Accordion.vue
+++ components/atoms/Accordion.vue
@@ -1,12 +1,11 @@
 <template>
-    <div id="accordion" class="accordion">
+    <span>
         <div
             class="accordion-header collapsed"
-            id="heading1"
             data-toggle="collapse"
-            data-target="#collapse1"
             aria-expanded="false"
-            aria-controls="collapse1"
+            :data-target="`#${name}`"
+            :aria-controls="name"
         >
             <h5>
                 {{ title }}
@@ -14,109 +13,112 @@
             </h5>
         </div>
         <div
-            id="collapse1"
-            class="collapse"
-            aria-labelledby="heading1"
+            class="collapse accordion-body"
             data-parent="#accordion"
-        >{{ message }}</div>
-    </div>
+            :id="name"
+        >
+            <slot></slot>
+        </div>
+    </span>
 </template>
 
 <script>
 export default {
     props: {
-        title: "",
-        message: "",
+        name: String,
+        title: String,
     },
 };
 </script>
 
 <style lang="scss" scoped>
-.accordion {
-    .accordion-header {
-        position: relative;
-        cursor: pointer;
-        padding: 0.75rem 0;
+.accordion-header {
+    position: relative;
+    cursor: pointer;
+    padding: 0.75rem 0;
 
+    .icon-toggle {
+        color: #86d0c1;
+
+        &::before {
+            top: 1rem;
+            right: 0.5rem;
+            content: "";
+            position: absolute;
+            display: block;
+            width: 15px;
+            height: 2px;
+            font-size: 1rem;
+            -webkit-transition: all 0.4s;
+            -moz-transition: all 0.4s;
+            -o-transition: all 0.4s;
+            transition: all 0.4s;
+        }
+
+        &::after {
+            top: 1rem;
+            right: 0.5rem;
+            content: "";
+            position: absolute;
+            display: block;
+            width: 15px;
+            height: 2px;
+            font-size: 1rem;
+            -webkit-transition: all 0.4s;
+            -moz-transition: all 0.4s;
+            -o-transition: all 0.4s;
+            transition: all 0.4s;
+        }
+    }
+
+    h5 {
+        margin-bottom: 0;
+        padding-right: 2rem;
+        font-size: 1.25rem;
+
+        .icon-toggle:before {
+            top: 1.5rem;
+        }
+
+        .icon-toggle:after {
+            top: 1.5rem;
+        }
+    }
+
+    &[aria-expanded="false"] {
         .icon-toggle {
-            color: #86d0c1;
-
             &::before {
-                top: 1rem;
-                right: 0.5rem;
-                content: "";
-                position: absolute;
-                display: block;
-                width: 15px;
-                height: 2px;
-                font-size: 1rem;
-                -webkit-transition: all 0.4s;
-                -moz-transition: all 0.4s;
-                -o-transition: all 0.4s;
-                transition: all 0.4s;
+                background: #999;
+                transform: rotate(-180deg);
             }
 
             &::after {
-                top: 1rem;
-                right: 0.5rem;
-                content: "";
-                position: absolute;
-                display: block;
-                width: 15px;
-                height: 2px;
-                font-size: 1rem;
-                -webkit-transition: all 0.4s;
-                -moz-transition: all 0.4s;
-                -o-transition: all 0.4s;
-                transition: all 0.4s;
-            }
-        }
-
-        h5 {
-            margin-bottom: 0;
-            padding-right: 2rem;
-            font-size: 1.25rem;
-
-            .icon-toggle:before {
-                top: 1.5rem;
-            }
-
-            .icon-toggle:after {
-                top: 1.5rem;
-            }
-        }
-
-        &[aria-expanded="false"] {
-            .icon-toggle {
-                &::before {
-                    background: #999;
-                    transform: rotate(-180deg);
-                }
-
-                &::after {
-                    background: #999;
-                    transform: rotate(-90deg);
-                }
-            }
-        }
-
-        &[aria-expanded="true"] {
-            h5 {
-                color: #86d0c1;
-            }
-
-            .icon-toggle {
-                &::before {
-                    background: #86d0c1;
-                    transform: rotate(0deg);
-                }
-
-                &::after {
-                    background: #86d0c1;
-                    transform: rotate(0deg);
-                }
+                background: #999;
+                transform: rotate(-90deg);
             }
         }
     }
+
+    &[aria-expanded="true"] {
+        h5 {
+            color: #86d0c1;
+        }
+
+        .icon-toggle {
+            &::before {
+                background: #86d0c1;
+                transform: rotate(0deg);
+            }
+
+            &::after {
+                background: #86d0c1;
+                transform: rotate(0deg);
+            }
+        }
+    }
+}
+.accordion-body {
+    color: #999;
+    font-size: 0.9rem;
 }
 </style>
