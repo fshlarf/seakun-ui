7b5dacf8606e7ac675166a78042ed547f7dcfa65
ahmadf04 <ahmad.faisal@elevenia.co.id>
2020-10-06 19:54:17 +0700

    update scheme of price page

--- pages/info/scheme-of-price/index.vue
+++ pages/info/scheme-of-price/index.vue
@@ -3,33 +3,28 @@
         <Header />
         <div class="scheme-price">
             <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <Title title="Bagaimana Seakun.id mengatur harga?" />
-                        <div class="card" v-for="(item, index) in providerList" :key="index">
-                            <img :src="item.img" alt="image not found" />
-                            <div class="row mt-3">
-                                <div class="col">
-                                    <p class="font-weight-bold">Detail Harga</p>
-                                    <div class="row" v-for="(info, ind) in item.informations" :key="ind">
-                                        <div :class="`col-md-auto ${info.is_total ? 'font-weight-bold' : ''}`">
-                                            {{ info.title }}
-                                        </div>
-                                        <div :class="`col text-right ${info.is_total ? 'font-weight-bold' : ''}`">
-                                            {{ info.price }}
-                                        </div>
+                <Title title="Bagaimana Seakun.id mengatur harga?" />
+                <div class="row justify-content-around content-mobile">
+                    <div v-for="(item, index) in providerList" :key="index">
+                        <div class="card">
+                            <img :src="item.img" alt="image not found" class="logo" />
+                            <img :src="item.screenshot" alt="image not found" class="ss" />
+                                <p class="font-weight-bold">Detail Harga</p>
+                                <div class="row" v-for="(info, ind) in item.informations" :key="ind">
+                                    <div :class="`col ${info.is_total ? 'font-weight-bold' : ''}`">
+                                        {{ info.title }}
+                                    </div>
+                                    <div :class="`col text-right ${info.is_total ? 'font-weight-bold' : ''}`">
+                                        {{ info.price }}
                                     </div>
                                 </div>
-                                <span class="divider"></span>
-                                <div class="col">
-                                    <p class="font-weight-bold">Skema Berlangganan</p>
-                                    <ol style="padding-left: 1rem;">
-                                        <li v-for="(scheme, indx) in item.schemes" :key="indx">
-                                            {{ scheme }}
-                                        </li>
-                                    </ol>
-                                </div>
-                            </div>
+                            <div class="divider"></div>
+                                <p class="font-weight-bold">Skema Berlangganan</p>
+                                <ol style="padding-left: 1rem;">
+                                    <li v-for="(scheme, indx) in item.schemes" :key="indx">
+                                        {{ scheme }}
+                                    </li>
+                                </ol>
                         </div>
                     </div>
                 </div>
@@ -43,6 +38,7 @@
 import Header from "~/components/mollecules/Header"
 import Footer from "~/components/organisms/Footer"
 import Title from "~/components/atoms/Title"
+import { providerList } from "./provider-list"
 
 export default {
     components: {
@@ -52,104 +48,7 @@ export default {
     },
     data() {
         return {
-            providerList: [
-                {
-                    img: '/images/netflix.png',
-                    schemes: [
-                        'Seakun membuat akun dan membeli Paket Premium di Netflix.',
-                        'Dalam 1 akun tersebut, Seakun membuat profile sebanyak 4.',
-                        'Profile disesuaikan dengan nama masing-masing member.',
-                        'Profile dilengkapi PIN yang berbeda-beda.',
-                        'Akun dishare HANYA ke 4 member dalam 1 grup.'
-                    ],
-                    informations: [
-                        {
-                            title: 'Harga Paket Premium Netflix',
-                            price: 'Rp186.000'
-                        },
-                        {
-                            title: 'Jumlah Member Dalam 1 Grup',
-                            price: '4'
-                        },
-                        {
-                            title: 'Harga Patungan',
-                            price: 'Rp186.000 ÷ 4 = Rp46.500'
-                        },
-                        {
-                            title: 'Biaya Admin Seakun',
-                            price: 'Rp6.500'
-                        },
-                        {
-                            title: 'Harga Paket Bulanan',
-                            price: 'Rp53.000',
-                            is_total: true
-                        },
-                    ]
-                },
-                {
-                    img: '/images/gramedia.png',
-                    schemes: [
-                        'Seakun membuat akun dan membeli Paket Premium di Gramedia.',
-                        'Akun dishare HANYA ke 5 member dalam 1 grup',
-                    ],
-                    informations: [
-                        {
-                            title: 'Harga Paket Premium Gramedia Digital',
-                            price: 'Rp89.000'
-                        },
-                        {
-                            title: 'Jumlah member dalam 1 grup',
-                            price: '5'
-                        },
-                        {
-                            title: 'Harga Patungan',
-                            price: 'Rp89.000 ÷ 5 = Rp17.800 ~Rp18.000'
-                        },
-                        {
-                            title: 'Biaya Admin Seakun',
-                            price: 'Rp5.000'
-                        },
-                        {
-                            title: 'Harga Paket Bulanan',
-                            price: 'Rp23.000',
-                            is_total: true
-                        },
-                    ]
-                },
-                {
-                    img: '/images/spotify.png',
-                    schemes: [
-                        'Seakun membuat akun dan membeli Paket Premium di Spotify',
-                        'Seakun bertindak sebagai user Host dalam grup',
-                        'Member membuat/menyediakan akun masing-masing',
-                        'Seakun mengundang 5 member untuk join 1 grup Premium',
-                        'Member yang telah join ke grup dapat menikmati fitur Premium'
-                    ],
-                    informations: [
-                        {
-                            title: 'Harga Paket Premium Netflix',
-                            price: 'Rp86.900'
-                        },
-                        {
-                            title: 'Jumlah Member Dalam 1 Grup',
-                            price: '5'
-                        },
-                        {
-                            title: 'Harga Patungan',
-                            price: 'Rp86.900 ÷ 5 = Rp17.380 ~Rp17.500 '
-                        },
-                        {
-                            title: 'Biaya Admin Seakun',
-                            price: 'Rp5.000'
-                        },
-                        {
-                            title: 'Harga Paket Bulanan',
-                            price: 'Rp22.500',
-                            is_total: true
-                        },
-                    ]
-                }
-            ]
+            providerList
         }
     }
 }
@@ -160,8 +59,10 @@ export default {
     padding: 100px 0px 40px !important;
 
     .divider {
-        width: 1px;
+        height: 1px;
+        width: 100%;
         background: gray;
+        margin: 1rem 0;
     }
 
     .card {
@@ -169,15 +70,50 @@ export default {
         border: 1px solid #eeeeee;
         border-radius: 8px;
         box-shadow: 0px 0px 4px #dddddd;
-        margin-top: 3rem;
+        margin: 3rem 0 0;
 
-        img {
+        .ss {
+            display: flex;
+            align-self: center;
+            width: 100%;
+            height: 247px;
+            object-fit: contain;
+            margin: 2rem 0;
+        }
+
+        .logo {
             width: 129px;
             height: 39px;
+            object-fit: contain;
+            margin-left: -8px;
         }
     }
 }
 
 @media (max-width: 800px) {
+.scheme-price {
+    .content-mobile {
+        display: block !important;
+        width: 100%;
+        padding: 0 20px;
+        margin-left: 0 !important;
+    }
+
+    .info-price {
+        font-size: 12px;
+    }
+
+    .info {
+        font-size: 12px;
+        width: auto !important;
+        padding-right: 0 !important;
+    }
+
+    .divider {
+        width: 100%;
+        height: 1px;
+        margin: 1rem 0;
+    }
+}
 }
 </style>
---
+++ pages/info/scheme-of-price/provider-list.js
@@ -0,0 +1,101 @@
+export const providerList = [
+    {
+        img: '/images/netflix.png',
+        screenshot: '/images/ss-netflix.png',
+        schemes: [
+            'Seakun membuat akun dan membeli Paket Premium di Netflix',
+            'Dalam 1 akun tersebut, Seakun membuat profile sebanyak 4',
+            'Profile disesuaikan dengan nama masing-masing member',
+            'Profile dilengkapi PIN yang berbeda-beda',
+            'Akun dishare HANYA ke 4 member dalam 1 grup'
+        ],
+        informations: [
+            {
+                title: 'Paket Premium Netflix',
+                price: 'Rp186.000'
+            },
+            {
+                title: 'Jumlah Member / Grup',
+                price: '4'
+            },
+            {
+                title: 'Patungan',
+                price: 'Rp186.000 ÷ 4 = Rp46.500'
+            },
+            {
+                title: 'Biaya Admin',
+                price: 'Rp6.500'
+            },
+            {
+                title: 'Paket Bulanan',
+                price: 'Rp53.000',
+                is_total: true
+            },
+        ]
+    },
+    {
+        img: '/images/spotify.png',
+        screenshot: '/images/ss-spotify.png',
+        schemes: [
+            'Seakun membuat akun dan membeli Paket Premium di Spotify',
+            'Seakun bertindak sebagai user Host dalam grup',
+            'Member membuat/menyediakan akun masing-masing',
+            'Seakun mengundang 5 member untuk join 1 grup Premium',
+            'Member yang telah join ke grup dapat menikmati fitur Premium'
+        ],
+        informations: [
+            {
+                title: 'Paket Premium Spotify',
+                price: 'Rp86.900'
+            },
+            {
+                title: 'Jumlah Member / Grup',
+                price: '5'
+            },
+            {
+                title: 'Patungan',
+                price: 'Rp86.900 ÷ 5 = Rp17.380 ~Rp17.500'
+            },
+            {
+                title: 'Biaya Admin',
+                price: 'Rp5.000'
+            },
+            {
+                title: 'Paket Bulanan',
+                price: 'Rp22.500',
+                is_total: true
+            },
+        ]
+    },
+    {
+        img: '/images/gramedia.png',
+        screenshot: '/images/ss-gramedia.png',
+        schemes: [
+            'Seakun membuat akun dan membeli Paket Premium di Gramedia',
+            'Akun dishare HANYA ke 5 member dalam 1 grup',
+        ],
+        informations: [
+            {
+                title: 'Paket Premium Gramedia Digital',
+                price: 'Rp89.000'
+            },
+            {
+                title: 'Jumlah Member / Grup',
+                price: '5'
+            },
+            {
+                title: 'Patungan',
+                price: 'Rp89.000 ÷ 5 = Rp17.800 ~Rp18.000'
+            },
+            {
+                title: 'Biaya Admin',
+                price: 'Rp5.000'
+            },
+            {
+                title: 'Paket Bulanan',
+                price: 'Rp23.000',
+                is_total: true
+            },
+        ]
+    },
+]
