37064823d1b56117ccca7cd42e805f3b8f1d9a2b
lubisarieff <lubisarieff@gmail.com>
2020-12-03 23:44:12 +0700

    update info payment process netflix

--- pages/info/payment-process/netflix.vue
+++ pages/info/payment-process/netflix.vue
@@ -2,36 +2,32 @@
 <div>
   <Header />
   <div class="netflix">
-      <Title title="Proses Pendaftaran dan Pembayaran Netflix" />
+    <Title title="Proses Pendaftaran dan Pembayaran Netflix" />
     <div class="container">
-        <div class="col">
-          <p>1&#41; Buka browser dan akses www.netflix.com </p>
-          <p>2&#41; Lakukan Pendaftaran dengan memasukkan alamat email di kolom email dan klik "Get Started"</p>
-            <img 
-            class="img-fluid"
+      <ul>
+        <li>Buka browser dan akses www.netflix.com</li>
+        <li>Lakukan Pendaftaran dengan memasukkan alamat email di kolom email dan klik "Get Started"</li>
+          <img 
             src="/images/netflix-get-started.png"
             alt="Image not found"/>
-          <p>3&#41; Di halaman Step 1, tambahkan password sesuai dengan ketentuan yang diinformasikan oleh Admin Seakun, kemudian klik "Continue"</p>
+        <li>Di halaman Step 1, tambahkan password sesuai dengan ketentuan yang diinformasikan oleh Admin Seakun, kemudian klik "Continue"</li>
           <img 
-            class="img-fluid"
             src="/images/netflix-register-1.png"
             alt="Image not found"/>
-          <p>4&#41; Di halaman Step 2, pilih plan sesuai informasi dari Admin Seakun, kemudian klik "Continue"</p>
-          <p>5&#41; Di halaman Step 3, masukkan informasi kartu kredit atau debit international kamu. Kemudian centang kotak "I agree" dan klik "Star Membership"</p>
+        <li>Di halaman Step 2, pilih plan sesuai informasi dari Admin Seakun, kemudian klik "Continue"</li>
+        <li>Di halaman Step 3, masukkan informasi kartu kredit atau debit international kamu. Kemudian centang kotak "I agree" dan klik "Star Membership"</li>
           <img 
-            class="img-fluid"
             src="/images/netflix-register-2.png"
             alt="Image not found"/>
-          <p>6&#41; Jika proses pembayaran berhasil, saldo di kartu kamu akan terpotonh sebesar harga plan yang dipiluh dan kamu akan diarahkan ke halaman pengaturan profil</p>
-          <p>7&#41; Isi profil menggunakan nama "Seakun" dan klik "Continue". Lakukan pengaturan bahasa dan preferensi (jika muncul)</p>
-          <p>8&#41; Setelah berada di Home, klik icon profil dan buka halaman Accoung > Billing Details (https://www.netflix.com/BillingActivity)</p>
-          <p>9&#41; Pastikan riwayat pembayaran telah muncul di halaman Billing Details</p>
+        <li>Jika proses pembayaran berhasil, saldo di kartu kamu akan terpotonh sebesar harga plan yang dipiluh dan kamu akan diarahkan ke halaman pengaturan profil</li>
+        <li>Isi profil menggunakan nama "Seakun" dan klik "Continue". Lakukan pengaturan bahasa dan preferensi (jika muncul)</li>
+        <li>Setelah berada di Home, klik icon profil dan buka halaman Accoung > Billing Details (https://www.netflix.com/BillingActivity)</li>
+        <li>Pastikan riwayat pembayaran telah muncul di halaman Billing Details</li>
           <img 
-            class="img-fluid"
             src="/images/netflix-payment-info.png"
             alt="Image not found"/>
-          <p>10&#41; Informasikan ke Admin Seakun bahwa proses pendaftaran dan pembayaran telah berhasil. Admin akan melakukan pengecekan dan pengaturan lebih lanjut terhadap akun yang didaftarkan</p>
-        </div>
+        <li>Informasikan ke Admin Seakun bahwa proses pendaftaran dan pembayaran telah berhasil. Admin akan melakukan pengecekan dan pengaturan lebih lanjut terhadap akun yang didaftarkan</li>
+      </ul>
     </div>
   </div>
   <Footer />
@@ -54,28 +50,28 @@ export default {
 
 <style lang="scss" scoped>
 .netflix {
-  padding: 60px 0px 80px !important;
+  padding: 80px 10px 80px !important;
   background: white;
   .container {
-    background-color: white;
-    padding: 15px 0px 15px !important;
-  }
-  .col {
-    padding: 0px 0px 0px !important;
-    p {
-      color:#1F2023;
-      text-align: justify;
-      margin-left: -15px;
-      margin-right: -15px;
-      margin-top: 12px;
-      margin-bottom: 12px;
-    }
-    img {
-      padding: 25px 0px 20px !important;
-      max-width: 80%;
-      max-height: 75%;
-      display: block;
-      margin: 0 auto;
+    padding: 25px 0px 25px !important;
+    ul {
+      list-style: number;
+      padding: 0px 10px 10px !important;
+      li {
+        color:#1F2023;
+        text-align: justify;
+        margin-top: 12px;
+        margin-bottom: 12px;
+        margin-left: -15px;
+        margin-right: -15px;
+      }
+      img {
+        padding: 25px 0px 25px !important;
+        max-width: 100%;
+        max-height: 100%;
+        display: block;
+        margin: 0 auto;
+      }
     }
   }
 }
