37a4c8954980fa7e83294734903f165d42b194ae
lubisarieff <lubisarieff@gmail.com>
2020-12-07 09:20:47 +0700

    add info spotify-payment

--- pages/info/payment-process/spotify.vue
+++ pages/info/payment-process/spotify.vue
@@ -1,13 +1,113 @@
 <template>
-  <div>spotify</div>
+  <div>
+    <Header />
+    <div class="spotify">
+      <div class="container">
+        <Title title="Proses Pembayaran di Spotify"/>
+        <div class="row justify-content-around content-mobile">
+          <ul>
+            <li>Buka browser dan akses www.Spotify.com</li>
+            <li>Klik Menu “Masuk”</li>
+            <img 
+              src="/images/spotify-home.png" 
+              alt="Image not found">
+            <li>Login menggunakan akun yg diberikan admin Seakun</li>
+            <img 
+              src="/images/spotify-login.png"
+              alt="Image not found">
+            <li>Pilih Akun</li>
+            <img 
+              src="/images/spotify-profile.png"
+              alt="Image not found">
+            <li>Klik Gabung Premium</li>
+            <img 
+              src="/images/spotify-dashboard.png"
+              alt="Image not found">
+            <li>Ubah paket jika terpilih paket personal dengan cara klik “Ubah Paket”</li>
+            <img 
+              src="/images/spotify-premium.png"  
+              alt="Image not found">
+            <li>Pilih Paket “Family” dan klik “Memulai”</li>
+            <img 
+              src="/images/spotify-info.png"
+              alt="Image not found">
+            <li>Klik “Pakai Gopay, DANA”</li>
+            <img 
+              src="/images/spotify-payment.png"
+              alt="Image not found">
+            <li>Lakukan pembayaran menggunakan Gopay/DANA milik kamu</li>
+            <li>Kirim bukti berhasil payment ke admin (Screenshot)</li>
+            <li>Logout akun tersebut. Admin akan melakukan pengecekan dan pengaturan lebih lanjut terhadap akun yang didaftarkan</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
-export default {
+import Header from "~/components/mollecules/Header";
+import Footer from "~/components/mollecules/Footer";
+import Title from "~/components/atoms/Title";
 
-}
+export default {
+  components: {
+    Header,
+    Title,
+    Footer
+  },
+};
+
 </script>
 
-<style>
+<style lang="scss" scoped>
+.spotify {
+  padding: 80px 10px 80px !important;
+  background: white;
+  .container {
+    padding: 25px 0px 25px !important;
+    ul {
+      counter-reset: number;
+      list-style: none;
+      padding: 0px 10px 10px !important;
+      li {
+        color:#1F2023;
+        text-align: justify;
+        margin-top: 12px;
+        margin-bottom: 12px;
+        margin-left: -15px;
+        margin-right: -15px;
+        display: flex;
+      }
+      li:before {
+        content: counter(number)")";
+        counter-increment: number;
+        padding-right: 5px;
+        
+      }
+      img {
+        padding: 25px 0px 25px !important;
+        max-width: 100%;
+        max-height: 100%;
+        display: block;
+        margin: 0 auto;
+      }
+    }
+  }
+}
 
+@media (max-width: 800px) {
+  .spotify {
+    .content-mobile {
+      display: block !important;
+      width: 100%;
+      padding: 0 0px;
+      margin-left: 0 !important;
+      img {
+        margin: 0 0;
+      }
+    }
+  }
+}
 </style>
