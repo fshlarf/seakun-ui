5ab05b0f43669a9a98330e58681194b7974cb92b
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-06-18 10:44:16 +0700

    [RLP][REDO NAVBAR USING TAILWIND]

--- components/mollecules/Navbar.vue
+++ components/mollecules/Navbar.vue
@@ -1,41 +1,66 @@
 <template>
-  <nav class="navbar navbar-expand-lg">
-    <div class="container nav-container">
-      <nuxt-link class="navbar-brand d-flex" to="/">
-        <MainLogo label="Seakun.id" />
+  <div id="navbar" class="w-full fixed z-40 py-2 tn:shadow px-2">
+    <div v-if="open" class="opacity-20 fixed inset-0 z-90 bg-black"></div>
+    <div
+      class="container static z-0 w-full text-gray-700 md:px-10 lg:flex lg:justify-between lg:items-center"
+    >
+      <nuxt-link class="hover:no-underline" to="/">
+        <Logo label="Seakun.id" />
       </nuxt-link>
-      <button
-        class="navbar-toggler collapsed"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-expanded="false"
-        aria-controls="navbarSupportedContent"
-        aria-label="Toggle navigation"
+      <div
+        class="absolute top-1 right-1 tn:py-3 tn:px-3 md:py-3 md:px-3 md:right-1 lg:top-0 lg:right-0 lg:relative z-100 flex flex-col w-4/5 md:w-1/2 lg:w-4/5 rounded-xl lg:p-0 lg:justify-end lg:flex-row"
+        :class="{ shadow: open, 'bg-white': open }"
       >
-        <span class="sr-only">Toggle Navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+        <div class="items-center">
+          <button
+            class="lg:hidden rounded-lg focus:outline-none float-right"
+            @click="open = !open"
+          >
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              class="w-6 h-6 primary"
+            >
+              <path
+                v-show="!open"
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+              <path
+                v-show="open"
+                fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <div class="mr-auto"></div>
-        <ul class="nav navbar-nav">
-          <li class="nav-item" v-for="(navbar, id) in navbarLink" :key="id">
-            <a class="nav-link text-primary">{{ navbar.label }}</a>
-          </li>
-        </ul>
+        <nav
+          :class="{ flex: open, hidden: !open }"
+          class="flex-col tn:pr-4 md:pr-4 tn:mt-2 md:p-0 hidden lg:flex lg:justify-end lg:flex-row"
+          v-for="(navbar, id) in navbarLink"
+          :key="id"
+        >
+          <p
+            class="cursor-pointer text-right my-3 text-sm md:text-base font-semibold md:font-bold text-primary hover:text-secondary rounded-lg md:py-0 md:mt-0 hover:opacity-50 focus:opacity-50 lg:ml-12 xl:ml-16"
+            href="#"
+          >
+            {{ navbar.label }}
+          </p>
+        </nav>
       </div>
     </div>
-  </nav>
+  </div>
 </template>
 
 <script>
-import MainLogo from '~/components/atoms/MainLogo.vue';
+import Logo from '~/components/atoms/Logo.vue';
 export default {
   data() {
     return {
+      open: false,
       navbarLink: [
         {
           id: 1,
@@ -66,105 +91,20 @@ export default {
     };
   },
   components: {
-    MainLogo,
-  },
-  methods: {
-    hideMenu() {
-      let menuContainer = document.getElementById('navbarSupportedContent');
-      menuContainer.classList.remove('show');
-    },
+    Logo,
   },
 };
 </script>
 
-<style lang="scss" scoped>
-.navbar {
-  background-color: #f4f9f8 !important;
-  position: fixed;
-  width: 100%;
-  z-index: 100;
-
-  .nav-link {
-    font-weight: 700;
-    margin-left: 50px;
-
-    &:hover {
-      cursor: pointer;
-      background-color: rgba(255, 255, 255, 0.1);
-      border-radius: 4px;
-    }
-  }
-
-  .navbar-brand {
-    cursor: pointer;
-    span {
-      color: #ffffff;
-      font-weight: 700;
-    }
+<style>
+@media (min-width: 400px) {
+  #navbar {
+    background-color: #f4f9f8;
   }
 }
-
-@media only screen and (min-device-width: 1000px) and (max-device-width: 1034px) {
-  .nav-container {
-    padding: 0 1rem 0 1rem;
-  }
-
-  .nav-link {
-    margin-left: 1px;
-  }
-}
-
-@media (max-width: 840px) {
-  .navbar {
-    position: fixed;
-    width: 100%;
-    z-index: 100;
-
-    .nav-container {
-      padding: 0 1rem 0 1rem;
-    }
-
-    .nav-link {
-      margin-left: 0;
-    }
-
-    .navbar-link {
-      padding-left: 20px;
-    }
-
-    .navbar-toggler {
-      background-color: transparent !important;
-      border-radius: 2px !important;
-      border: 1px solid #86d0c1;
-      height: 34px;
-
-      span {
-        &.icon-bar {
-          display: block;
-          width: 14px;
-          height: 2px;
-          border-radius: 1px;
-          background: #86d0c1;
-          margin: 2px 0px;
-        }
-      }
-    }
-  }
-}
-
-@media (max-width: 400px) {
-  .navbar {
-    position: fixed;
-    width: 100%;
-    z-index: 100;
-
-    .nav-container {
-      padding: 0;
-    }
-
-    .nav-link {
-      margin-left: 0;
-    }
+@media (max-width: 800px) {
+  nav {
+    text-transform: uppercase;
   }
 }
 </style>
