feecb08827bc320bafc2657eeb942edc76d6f932
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-06-23 11:45:30 +0700

    [RLP-15][SQ][RESOLVE NAVBAR BLANK]

--- components/mollecules/NavbarBlank.vue
+++ components/mollecules/NavbarBlank.vue
@@ -1,10 +1,7 @@
 <template>
-  <div
-    id="navbar"
-    class="w-full bg-white fixed z-40 tn:py-1 lg:py-2 tn:shadow px-2"
-  >
+  <div id="navbar" class="w-full bg-third fixed z-40 py-2 tn:shadow px-2">
     <div
-      class="container static z-0 w-full text-gray-700 md:px-10 lg:flex lg:justify-between lg:items-center"
+      class="container static z-0 w-full text-gray-700 md:px-10 lg:flex lg:justify-between"
     >
       <nuxt-link class="hover:no-underline" to="/">
         <Logo label="Seakun.id" />
@@ -16,10 +13,29 @@
 <script>
 import Logo from '~/components/atoms/Logo.vue';
 export default {
+  name: 'navbar-blank',
+  data() {
+    return {};
+  },
   components: {
     Logo,
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+#navbar {
+  position: fixed;
+  top: 1px;
+}
+@media (min-width: 400px) {
+  #navbar {
+    background-color: #f4f9f8;
+  }
+}
+@media (max-width: 800px) {
+  nav {
+    text-transform: uppercase;
+  }
+}
+</style>
