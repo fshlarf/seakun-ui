88a3ad6f2a680d13fd33579eb6599f9d367d3597
faishal.arif <faishal.arif@thebonza.com>
2021-06-26 22:00:52 +0700

    update HexCard class

--- components/atoms/HexCard.vue
+++ components/atoms/HexCard.vue
@@ -3,10 +3,10 @@
     <div
       class="text-center absolute z-100 mx-auto left-0 right-0 top-16 lg:top-24"
     >
-      <h1 class="text-xl lg:text-xl xl:text-2xl font-bold">
+      <h1 class="tn:text-3xl md:text-4xl font-bold">
         {{ value }}
       </h1>
-      <p class="text-base md:text-md xl:text-2xl font-bold">{{ label }}</p>
+      <p class="md:text-base">{{ label }}</p>
     </div>
     <img class="static z-0 w-100 h-100" src="/images/polygon-bg.svg" alt="" />
   </div>
