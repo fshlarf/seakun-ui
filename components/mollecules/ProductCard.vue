804807b3cd24416cc613733abdcafaeec0086311
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-06-29 14:57:05 +0700

    fix disney hotstar banner card image

--- components/mollecules/ProductCard.vue
+++ components/mollecules/ProductCard.vue
@@ -7,7 +7,7 @@
         v-if="product.slug === 'disney-hotstar'"
         :src="`/images/product/${product.slug}.png`"
         alt="image not found"
-        class="object-cover rounded-br-3xl max-h-[185px] lg:rounded-br-none tn:w-auto md:w-full tn:h-[8.5rem] md:h-full"
+        class="object-cover rounded-br-3xl max-h-[185px] lg:rounded-br-none tn:w-auto md:w-full tn:h-[8rem] md:h-full"
       />
       <img
         v-else
