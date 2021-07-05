228790fd2e27349805172b5aa7ed4ab80c7a5c90
faishal.arif <faishal.arif@thebonza.com>
2021-07-05 21:21:41 +0700

    update layout tos

--- pages/terms-of-use.vue
+++ pages/terms-of-use.vue
@@ -239,7 +239,7 @@ import Header from '~/components/mollecules/Header';
 import Footer from '~/components/mollecules/Footer';
 export default {
   name: 'ToSPage',
-  layout: 'navigationBlank',
+  layout: 'new',
   components: {
     Header,
     Footer,
