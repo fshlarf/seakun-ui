864d2a225e3ab1d95dd0d45b2fd549d1d8cc7f6b
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-07-25 17:52:21 +0700

    recon thankyou page following figma updated design

--- pages/thankyou/index.vue
+++ pages/thankyou/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="thankyou max-w-2xl w-full mx-auto mt-10 py-4">
-    <div class="thankyou-container shadow-md px-4 pb-10 w-full mt-10 pt-4">
+  <div class="thankyou max-w-2xl w-full mx-auto py-4">
+    <div
+      class="thankyou-container md:rounded-3xl md:shadow-md tn:px-3 md:px-8 md:py-8 w-full pt-4"
+    >
       <img
         class="w-6/12 mx-auto"
         src="/images/thank-you-new.png"
@@ -13,59 +15,113 @@
           untuk memberikan detail pesananmu.
         </p>
       </div>
-      <div class="detail-order bg-white px-12 mt-12 py-4">
-        <div class="border-b-2 border-gray-200 pb-3">
-          <p class="text-lg text-gray-400">Metode Pembayaran</p>
-          <div class="flex justify-between items-center mt-2" v-if="bankImage">
-            <p class="font-bold text-lg">Bank Transfer Manual</p>
-            <img class="w-3/12" @onerror="onErrorImageBank()" :src="`images/payment/${bankImage}.png`" />
-          </div>
-            <div class="flex justify-between items-center mt-2" v-else>
-            <p class="font-bold text-lg">Bank Transfer Manual - <span class="capitalize">{{paymentBank}}</span></p>
-          </div>
-        </div>
-        <div class="mt-4">
-          <p class="text-lg text-gray-400">Bank Tujuan</p>
-          <p class="font-bold text-lg mt-2">
-            {{ destinationBank }} a.n PT. Seakun Global
-          </p>
-        </div>
-        <div class="mt-4">
+
+      <div
+        class="detail-order rounded-2xl shadow-md bg-white tn:px-4 md:px-8 md:mx-8 mt-8 py-4"
+      >
+        <div class="">
           <p class="text-lg text-gray-400">Nama Rekening</p>
-          <p class="font-bold text-lg mt-2">{{ paymentHolder }}</p>
+          <p class="font-bold text-lg md:mt-2">{{ paymentHolder }}</p>
+        </div>
+        <div class="border-b-2 border-gray-200 tn:pb-1 md:pb-3 tn:mt-3 md:mt-4">
+          <p class="text-lg text-gray-400">Metode Pembayaran</p>
+          <div v-if="paymentBankFrom === 'lainnya'" class="md:mt-2">
+            <p class="font-bold text-lg">Bank Transfer Manual</p>
+          </div>
+          <div
+            v-else
+            class="tn:grid tn:grid-cols-5 tn:justify-between tn:items-center md:mt-2 relative"
+          >
+            <p class="font-bold text-lg tn:col-span-3">Bank Transfer Manual</p>
+            <img
+              v-if="paymentBankFrom"
+              class="tn:col-span-2 absolute bottom-0 right-0 tn:w-1/3 tn:h-auto md:w-auto md:h-16"
+              @onerror="onErrorImageBank()"
+              :src="`images/payment/${setNameBank(paymentBankFrom)}.png`"
+            />
+          </div>
+        </div>
+
+        <div class="tn:mt-3 md:mt-4">
+          <p class="text-lg text-gray-400">Bank Tujuan</p>
+          <div
+            class="tn:grid tn:grid-cols-5 tn:justify-between tn:items-center md:mt-2 relative"
+          >
+            <p class="font-bold text-lg tn:col-span-3">
+              {{ destinationBank }} a.n PT. Seakun Global
+            </p>
+            <img
+              v-if="paymentBankTo"
+              class="tn:col-span-2 absolute bottom-0 right-0 tn:w-1/3 tn:h-auto md:w-auto md:h-16"
+              @onerror="onErrorImageBank()"
+              :src="`images/payment/${setNameBank(paymentBankTo)}.png`"
+            />
+          </div>
         </div>
       </div>
+      <div class="">
+        <p class="tn:text-center md:text-left text-lg mt-8 text-gray-500">
+          Mohon menunggu 10-20 menit. Jika melewati rentang waktu tersebut dan
+          pesanan kamu belum di proses, harap hubungi admin via whatsapp
+          <a
+            class="text-primary"
+            target="_blank"
+            href="https://api.whatsapp.com/send?phone=6282124852227"
+            >+6282124852227</a
+          >
+        </p>
+      </div>
+      <Button
+        class="w-full bg-green-seakun text-white mt-3 py-2"
+        label="Kembali ke beranda"
+        @click="toHomePage()"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import Button from '~/components/atoms/Button';
 export default {
   name: 'thankyou-page',
   layout: 'navigationBlank',
   data() {
     return {
       paymentHolder: '',
-      paymentBank: '',
+      paymentBankFrom: '',
+      paymentBankTo: '',
       destinationBank: '',
-      paymentImage: '',
-      availableImagePayment : [
-        'dana',
-        'bca',
-        'bri',
-        'gopay',
-        'link-aja',
-        'mandiri',
-        'ovo'
-      ]
     };
+  },
+  components: {
+    Button,
   },
   mounted() {
     const { holder, to, from } = this.$router.history.current.query;
     this.paymentHolder = holder;
     this.destinationBank = to;
-    this.paymentBank = from.toLowerCase();
-    this.bankImage = this.availableImagePayment.find((bank)=> bank == this.paymentBank )
+    this.paymentBankFrom = from.toLowerCase();
+    this.paymentBankTo = to.toLowerCase();
+  },
+  methods: {
+    setNameBank(bank) {
+      switch (bank) {
+        case 'digibank / dbs':
+          return 'digibank';
+          break;
+        case 'keb hana / line bank':
+          return 'keb hana';
+          break;
+        case 'syariah indonesia / bsi':
+          return 'bsi';
+          break;
+        default:
+          return bank;
+      }
+    },
+    toHomePage() {
+      this.$router.push('/');
+    },
   },
 };
 </script>
@@ -75,12 +131,6 @@ export default {
   background: rgba(254, 254, 254, 0.55);
   backdrop-filter: blur(90px);
   /* Note: backdrop-filter has minimal browser support */
-/* Note: backdrop-filter has minimal browser support */
-
-  border-radius: 30px;
-}
-.detail-order {
-  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.04);
-  border-radius: 10px;
+  /* Note: backdrop-filter has minimal browser support */
 }
 </style>
