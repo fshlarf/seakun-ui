a386e7039546c4ea7fb539c9e76227c981907e67
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-07-28 12:30:01 +0700

    implement constant api in customers info page

--- pages/info/customers/index.vue
+++ pages/info/customers/index.vue
@@ -70,6 +70,7 @@
 
 <script>
 import axios from 'axios';
+import { SEAKUN_API } from '~/constants/api.js';
 import NavbarBlank from '~/components/mollecules/NavbarBlank';
 import CardShimmer from '~/components/mollecules/CardShimmer';
 import Footer from '~/components/mollecules/Footer';
@@ -86,6 +87,7 @@ export default {
   },
   data() {
     return {
+      SEAKUN_API,
       customers: [],
       shimmerInitialData: Array(4),
       isLoading: false,
@@ -96,12 +98,11 @@ export default {
   },
   methods: {
     getCustomersData() {
+      const { SEAKUN_API } = this;
       let provider = this.$route.query.provider;
       this.isLoading = true;
       axios
-        .get(
-          `https://seakun-api.herokuapp.com/registered-user/group-${provider}`
-        )
+        .get(`${SEAKUN_API}/registered-user/group-${provider}`)
         .then((res) => this.processDataCustomers(res.data))
         .catch((err) => console.log(err))
         .finally(() => (this.isLoading = false));
