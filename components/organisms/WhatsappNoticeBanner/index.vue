7668b509cbd5f78c5b55f5037d6d18c90bc9581a
Januar Salman <januar@ruangguru.com>
2021-08-08 13:01:15 +0700

    add whatsapp notice

---
+++ components/organisms/WhatsappNoticeBanner/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="whatsapp-banner container iner tn:mt-2 lg:pt-10 pt-10">
+    <div class="bg-warning-seakun rounded-3xl d-flex xs:flex-row px-16 py-6 items-center">
+       <div class="whatsapp-banner-img ">  
+        <img class="h-56 w-auto" src="/images/illustration-whatsapp-banner.svg"/>
+      </div>
+      <div class="whatsapp-banner-content flex-1 px-8">
+        <h1 class="text-3xl font-bold text-green-seakun-dark mt-4 mb-4 ">  Info Penting </h1>   
+        <p class="font-bold mb-2 my-2">
+          Kepada pengguna Seakun, per-tanggal 8 Agustus 2021, WhatsApp Seakun.id yg sebelumnya (+6282124852227) mengalami kendala.
+        </p>
+    
+        <p class=" font-light my-2"
+          >segala proses pembayaran maupun pengaduan harap diinfokan ke nomor
+          whatsapp berikut
+          <a
+            href="https://api.whatsapp.com/send?phone=6282124852232"
+            class="text-green-seakun-dark"
+            >+6282124852232</a
+          >
+        </p>       
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/organisms/WhatsappNoticeModal/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <Modal
+      :is-show="isShowInfoModal"
+      size="20"
+      @onClose="isShowInfoModal = false"
+    >
+      <div class="max-w-xs d-flex flex-column justify-center items-center mt-2">
+        
+        <div class="notice-header w-full mb-2  d-flex flex-column justify-center	 items-center">
+            <img src="/images/illustration-whatsapp-notice.svg" class="w-6/12"/>
+            <h1 class="text-xl font-bold	 mt-4">  Info Penting </h1>          
+        </div>
+        <p class="font-light mb-2">
+          Kepada pengguna Seakun, per-tanggal 8 Agustus 2021, WhatsApp Seakun.id yg sebelumnya (+6282124852227) mengalami kendala.
+        </p>
+    
+        <b
+          >segala proses pembayaran maupun pengaduan harap diinfokan ke nomor
+          whatsapp berikut
+          <a
+            href="https://api.whatsapp.com/send?phone=6282124852232"
+            class="text-primary"
+            >+6282124852232</a
+          >
+        </b>
+      </div>
+    </Modal>
+</template>
+
+<script>
+import Modal from '~/components/atoms/Modal.vue';
+
+export default {
+    components: {
+        Modal
+    },
+    data() {
+        return {
+        isShowInfoModal: true,
+    };
+  },
+
+}
+</script>
+
+<style>
+
+</style>
