eac00e5548539343573f029d6e15e1b4e3e5244f
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-08-21 17:49:22 +0700

    [AYCE][Add Qna section]

--- pages/all-you-can-eat/index.vue
+++ pages/all-you-can-eat/index.vue
@@ -3,6 +3,7 @@
     <HeaderSection />
     <DetailMenuSection />
     <OrderFlowSection />
+    <QnaSection />
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 import HeaderSection from './views/HeaderSection.vue';
 import DetailMenuSection from './views/DetailMenuSection.vue';
 import OrderFlowSection from './views/OrderFlowSection.vue';
+import QnaSection from './views/QnaSection.vue';
 export default {
   components: {
     HeaderSection,
     DetailMenuSection,
     OrderFlowSection,
+    QnaSection,
   },
   layout: 'new',
 };
