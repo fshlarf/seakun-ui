6d043a41c4d580da355f3f87b8c38fffe30cce7c
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-08-21 17:46:53 +0700

    [AYCE][Add order flow section]

--- pages/all-you-can-eat/index.vue
+++ pages/all-you-can-eat/index.vue
@@ -2,16 +2,19 @@
   <div>
     <HeaderSection />
     <DetailMenuSection />
+    <OrderFlowSection />
   </div>
 </template>
 
 <script>
 import HeaderSection from './views/HeaderSection.vue';
 import DetailMenuSection from './views/DetailMenuSection.vue';
+import OrderFlowSection from './views/OrderFlowSection.vue';
 export default {
   components: {
     HeaderSection,
     DetailMenuSection,
+    OrderFlowSection,
   },
   layout: 'new',
 };
