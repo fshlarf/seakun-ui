06de809c9f317bdc45e2b47874a4ff686772a733
Ibnu Hazar <ibnuhazar101@gmail.com>
2021-11-22 16:57:17 +0700

    update resonsive menu location

--- pages/all-you-can-eat/views/LocationMenu.vue
+++ pages/all-you-can-eat/views/LocationMenu.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="md:my-4">
-    <div class="md:grid md:grid-cols-2 md:gap-8 tn:space-y-4 md:space-y-0">
+  <div class="my-4">
+    <div class="grid grid-cols-2 gap-2 md:gap-4 lg:gap-8">
       <div class="border">
         <div class="mapouter">
           <div class="gmap_canvas">
             <iframe
+              class="tn:h-[150px] md:h-[300px] lg:h-[400px]"
               width="100%"
-              height="400"
               id="gmap_canvas"
               :src="`https://maps.google.com/maps?q=${address}&t=&hl=id&z=13&ie=UTF8&iwloc=&output=embed`"
               frameborder="0"
@@ -14,28 +14,12 @@
               marginheight="0"
               marginwidth="0"
             ></iframe>
-            <a href="https://putlocker-is.org"></a><br /><style>
-              .mapouter {
-                position: relative;
-                text-align: right;
-                height: 400px;
-                width: 100%;
-              }</style
-            ><a href="https://www.embedgooglemap.net">google map for free</a
-            ><style>
-              .gmap_canvas {
-                overflow: hidden;
-                background: none !important;
-                height: 400px;
-                width: 100%;
-              }
-            </style>
           </div>
         </div>
       </div>
       <div class="space-y-2">
-        <p class="text-2xl font-bold">Detail Lokasi</p>
-        <p>
+        <p class="md:text-2xl font-bold">Detail Lokasi</p>
+        <p class="tn:text-xs md:text-base font-medium">
           Voyage at Harris Vertu Hotel Harmoni, <br />{{ restaurant.address }}
         </p>
       </div>
