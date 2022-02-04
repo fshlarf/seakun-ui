a57810d6f7b1c45a67085b3289796d9dfe289ed3
Ibnu Hazar <ibnuhazar101@gmail.com>
2022-02-04 17:21:56 +0700

    filter bank list in payment page

--- pages/payment/index.vue
+++ pages/payment/index.vue
@@ -126,6 +126,7 @@ export default {
         orderNumber: '',
       },
       detailPaymentSequrban: {},
+      activeSeakunPayment: ['Jenius', 'BCA', 'Mandiri'],
       paymentSeakunList: {
         data: [],
         loading: true,
@@ -195,9 +196,15 @@ export default {
         );
         if (fetchSeakunPayment.data) {
           const dataResult = fetchSeakunPayment.data.data;
+          const activePayment = [];
+          dataResult.forEach((element) => {
+            if (this.activeSeakunPayment.includes(element.bank)) {
+              activePayment.push(element);
+            }
+          });
           this.paymentSeakunList = {
             loading: false,
-            data: dataResult,
+            data: activePayment,
           };
         } else {
           throw new Error(fetchSeakunPayment);
