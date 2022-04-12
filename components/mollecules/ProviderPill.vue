c99d3975d72fd2ec25febf54cf85510f100beaf1
gusma296 <47033246+gusma296@users.noreply.github.com>
2022-04-12 17:34:08 +0700

    [LANDING-PAGE][IMP] change provider with api data, change api group with new api and add infinite scroll for pagination group (#182)
    * add new service for get customers by provider
    
    * change tab provider data from api
    
    * hide setitip banner
    
    * add infinite scroll group with new api
    
    * add icons for apple one premium & hide button next if data < 5
    
    * remove log
    
    Co-authored-by: Ibnu Hazar <ibnuhazar101@gmail.com>

--- components/mollecules/ProviderPill.vue
+++ components/mollecules/ProviderPill.vue
@@ -3,13 +3,13 @@
     class="max-w-sm w-72 h-16 py-3 px-3 tn:rounded-full lg:rounded-2xl shadow flex space-x-2 items-center justify-center"
     @click="selectProvider(provider)"
   >
-    <img :src="`${provider.icon}`" alt="#" />
+    <img :src="`/images/icons/${provider.slug}${provider.slug === 'disney-hotstar'? '.png' : '.svg'}`" alt="#" />
     <p class="text-lg font-bold">
       {{ provider.name }}
       <span
-        v-if="!isLoading && provider.slug === dataGroup[0].name"
+        v-if="!isLoading && dataGroup.length > 0 && provider.slug === dataGroup[0].providerSlug"
         class="text-sm font-normal"
-        >({{ dataGroup[0].groupNumber }} grup)</span
+        >({{ dataGroup[0].group }} grup)</span
       >
     </p>
   </div>
