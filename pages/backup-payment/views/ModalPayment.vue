35d5d32dd9cfced5ac5a3772515a1fdb0cdfcc82
Ibnu Hazar <ibnuhazar101@gmail.com>
2022-05-10 23:42:19 +0700

    add new payment page

---
+++ pages/backup-payment/views/ModalPayment.vue
@@ -0,0 +1,138 @@
+<template>
+  <Modal :is-show="showModal" @onClose="onClose" size="xl:w-2/4">
+    <template v-slot:header>
+      <div class="font-bold text-lg">Pilih Tipe Pembayaran</div>
+    </template>
+    <div class="inline-grid md:grid-cols-2 gap-4 py-2">
+      <div class="flex-1 shadow-md rounded-md p-1">
+        <p class="text-lg font-bold text-center">Pembayaran Otomatis</p>
+        <div class="mt-3 px-3">
+          <img class="w-full" src="/images/payment/auto-payment.png" />
+        </div>
+        <div class="flex flex-row mb-2 mt-4 mx-2">
+          <div class="mt-1">
+            <CheckBox />
+          </div>
+          <div>
+            <p class="ml-2 text-base font-normal">
+              QRIS, E-Wallet, berlaku disemua durasi berlangganan
+            </p>
+          </div>
+        </div>
+        <div class="flex flex-row mb-2 mx-2">
+          <div class="mt-1">
+            <CheckBox />
+          </div>
+          <div>
+            <p class="ml-2 text-base font-normal">
+              Virtual Account dan Retail hanya berlaku untuk durasi di atas 3
+              bulan berlangganan
+            </p>
+          </div>
+        </div>
+        <div class="flex flex-row mb-3 mx-2">
+          <div class="mt-1">
+            <CheckBox />
+          </div>
+          <div>
+            <p class="ml-2 text-base font-normal">
+              Konfirmasi pembayaran otomatis
+            </p>
+          </div>
+        </div>
+        <div class="px-2 pb-2">
+          <Button
+            label="Bayar Otomatis"
+            @click="onClickOtomatis"
+            class="w-full bg-green-seakun text-base text-white font-bold"
+          />
+        </div>
+      </div>
+      <div class="flex-1 shadow-md rounded-md p-1">
+        <p class="text-lg font-bold text-center">Pembayaran Manual</p>
+        <div class="mt-3 px-3">
+          <img class="mx-auto" src="/images/payment/manual-payment.png" />
+        </div>
+        <div class="flex flex-row mb-2 mt-4 mx-2">
+          <div class="mt-1">
+            <CheckBox class="w-4" />
+          </div>
+          <div>
+            <p class="ml-2 text-base font-normal">
+              Transfer manual ke Bank BCA, Mandiri dan jenius
+            </p>
+          </div>
+        </div>
+        <div class="flex flex-row mb-3 mx-2">
+          <div class="mt-1">
+            <CheckBox />
+          </div>
+          <div>
+            <p class="ml-2 text-base font-normal">
+              Konfirmasi pembayaran manual (upload bukti transfer di website)
+            </p>
+          </div>
+        </div>
+        <div class="px-2 pb-2">
+          <Button
+            label="Bayar Manual"
+            @click="onClickManual"
+            class="w-full bg-green-seakun text-base text-white font-bold"
+          />
+        </div>
+      </div>
+    </div>
+  </Modal>
+</template>
+<script>
+import Modal from '~/components/atoms/Modal';
+import CheckBox from '~/assets/images/icon/checkbox.svg?inline';
+import Button from '~/components/atoms/Button';
+
+export default {
+  name: 'ModalPayment',
+  components: {
+    Modal,
+    CheckBox,
+    Button,
+  },
+  props: {
+    showModal: {
+      type: Boolean,
+      default: false,
+    },
+    dataPaymentMethodOtomatis: {
+      type: Array,
+      default: () => [
+        'qris',
+        'ovo',
+        'dana',
+        'shopee pay',
+        'link aja',
+        'mandiri',
+        'bni',
+        'bri',
+        'permata',
+        'alfamart',
+      ],
+    },
+    dataPaymentMethodManual: {
+      type: Array,
+      default: () => ['bca', 'mandiri', 'jenius'],
+    },
+    onClickManual: {
+      type: Function,
+      default: () => {},
+    },
+    onClickOtomatis: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  methods: {
+    onClose() {
+      this.$emit('onClose');
+    },
+  },
+};
+</script>
---
+++ pages/backup-payment/views/PaymentDetail.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <div v-if="isLoading" class="text-center mt-4">
+      <p class="shimmer w-7/12"></p>
+      <p class="shimmer w-6/12"></p>
+      <p class="shimmer w-9/12"></p>
+    </div>
+    <div v-else class="flex flex-column mt-4 items-center">
+      <p class="text-base font-normal opacity-50 mt-4">Total bayar</p>
+      <div class="payment-detail__price flex cursor-pointer">
+        <p
+          class="text-2xl font-bold my-2 text-green-seakun"
+          v-html="formatCodePayment(paymentTotal)"
+        ></p>
+      </div>
+    </div>
+    <div class="mt-12">
+      <p class="text-xl text-center font-bold">
+        Metode Pembayaran yang Tersedia
+      </p>
+      <div class="mt-4 mx-auto w-full">
+        <img class="w-full" src="/images/payment/all-payment.png" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { currencyFormat } from '~/helpers/word-transformation.js';
+
+export default {
+  name: 'PaymentDetail',
+  data: () => ({
+    currencyFormat,
+  }),
+  props: {
+    paymentTotal: {
+      type: Number,
+      default: 0,
+    },
+    dataPaymentMethod: {
+      type: Array,
+      default: () => [
+        'qris',
+        'ovo',
+        'dana',
+        'shopee pay',
+        'link aja',
+        'bca',
+        'mandiri',
+        'bni',
+        'bri',
+        'permata',
+        'jenius',
+        'alfamart',
+      ],
+    },
+    copyNominal: {
+      type: Function,
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    formatCodePayment(value) {
+      if (value) {
+        const currency = this.currencyFormat(value);
+        return currency;
+      }
+      return '-';
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.payment-detail {
+  &__price {
+    svg {
+      width: 14px;
+      height: auto;
+      display: inline-block;
+      fill: #8dcabe;
+      margin-top: -4px;
+      margin-left: 12px;
+      cursor: pointer;
+    }
+  }
+}
+</style>
