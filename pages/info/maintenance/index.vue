61766deff51054211eb26160080be0651842633b
Ibnu Hazar <ibnuhazar101@gmail.com>
2022-05-12 20:04:06 +0700

    remove maintenance page

--- pages/info/maintenance/index.vue
+++ pages/info/maintenance/index.vue
@@ -41,9 +41,9 @@ export default {
   components: {
     Button,
   },
-  // beforeMount() {
-  //   this.$router.push('/');
-  // },
+  beforeMount() {
+    this.$router.push('/');
+  },
 };
 </script>
 
