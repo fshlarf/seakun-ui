bd7b71d46357fd854ece763331bc27ecc40f0fe9
ibnu hazar <69763294+ibnuhazar101@users.noreply.github.com>
2022-06-15 22:25:33 +0700

    [S][LANDING-PAGE][IMP] update zoom price scheme (#201)

---
+++ components/organisms/PaymentSection/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="payment" class="container pt-20">
+    <h2 class="text-xl md:text-2xl font-bold tn:text-center md:text-left mb-2">
+      Metode Pembayaran
+    </h2>
+    <div
+      class="w-full grid md:grid-cols-2 justify-around items-center md:gap-4 lg:gap-8 tn:px-2 md:px-0"
+    >
+      <div class="">
+        <p class="tn:text-sm md:text-md xl:text-lg font-semibold">
+          Bertransaksi di Seakun.id sudah sangat mudah karena dilengkapi dengan
+          beragam metode pembayaran. Mulai dari Virtual Account, E-wallet, QRIS
+          hingga retail.
+        </p>
+      </div>
+      <div class="w-full my-4 overflow-hidden">
+        <img
+          class="w-full"
+          src="/images/payment/all-payment.png"
+          alt="payment method"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
