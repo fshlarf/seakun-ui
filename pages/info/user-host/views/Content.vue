bdb141f366e58f5a863a1e21cb05d3762a7fe5f5
ibnu hazar <69763294+ibnuhazar101@users.noreply.github.com>
2022-07-21 17:39:09 +0700

    [S][LANDING-PAGE][IMP] update accordion in user host info page (#226)

--- pages/info/user-host/views/Content.vue
+++ pages/info/user-host/views/Content.vue
@@ -2,23 +2,64 @@
   <div class="container pt-10">
     <div class="md:grid lg:grid-cols-4 lg:gap-4 xl:gap-8 items-start">
       <div class="relative lg:hidden">
-        <h3 class="px-3 text-lg mb-2">Pilih Provider:</h3>
         <div class="rounded-xl p-3 bg-greeny" @click="onClickChooseProvider">
-          <div class="flex justify-center items-center space-x-2">
-            <img
-              :src="`/images/icons/${chosedProvider.slug}.svg`"
-              alt="icon provider"
-              class="w-9"
-            />
-            <p class="font-bold text-lg">
-              {{ chosedProvider.name }}
-            </p>
+          <div class="flex justify-between items-center">
+            <div class="flex items-center space-x-2">
+              <img
+                :src="`/images/icons/${chosedProvider.slug}.svg`"
+                alt="icon provider"
+                class="w-9"
+              />
+              <p class="font-bold text-lg">
+                {{ chosedProvider.name }}
+              </p>
+            </div>
+            <div v-if="!isShowDropDown" class="w-4 h-4">
+              <svg
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 330 330"
+                style="enable-background: new 0 0 330 330"
+                xml:space="preserve"
+              >
+                <path
+                  id="XMLID_225_"
+                  d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
+	                c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
+	                s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
+                />
+              </svg>
+            </div>
+            <div v-if="isShowDropDown" class="w-4 h-4">
+              <svg
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                viewBox="0 0 330 330"
+                style="enable-background: new 0 0 330 330"
+                xml:space="preserve"
+              >
+                <path
+                  id="XMLID_224_"
+                  d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394
+                  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393
+                  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"
+                />
+              </svg>
+            </div>
           </div>
         </div>
         <transition name="slide-up">
           <div
             v-if="isShowDropDown"
-            class="border p-4 space-y-4 mb-2 bg-white absolute z-50 top-28 w-full"
+            class="border p-4 space-y-4 mb-2 bg-white absolute z-50 top-18 w-full"
           >
             <p
               role="button"
