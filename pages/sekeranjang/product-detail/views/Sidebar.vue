04abbf16a8fb0956a4f5aedb673ef1969e5680ee
ibnu hazar <69763294+ibnuhazar101@users.noreply.github.com>
2022-10-17 19:23:56 +0700

    [S][LANDING-PAGE][IMP] sekeranjang - reduce font size in detail page (#268)

--- pages/sekeranjang/product-detail/views/Sidebar.vue
+++ pages/sekeranjang/product-detail/views/Sidebar.vue
@@ -7,10 +7,10 @@
       >
         {{ product.sekeranjang.productBrand }}
       </p>
-      <h1 class="text-[32px] font-semibold tracking-tight">
+      <h1 class="text-[24px] font-semibold tracking-tight">
         {{ product.name }}
       </h1>
-      <p class="text-[44px] font-bold tn:mt-3 tracking-tight">
+      <p class="text-[36px] font-bold tn:mt-3 tracking-tight">
         {{ currencyFormat(product.finalPrice) }}
       </p>
       <div class="flex items-center space-x-3 tn:mt-1">
