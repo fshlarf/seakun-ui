28b9af02699a250873af153115810f5d777e7f07
ibnu hazar <69763294+ibnuhazar101@users.noreply.github.com>
2022-10-27 10:58:30 +0700

    [M][LANDING-PAGE][IMP] update apple price (#269)

--- constants/price-change.js
+++ constants/price-change.js
@@ -1,32 +1,57 @@
 export const priceChangeList = [
+  // {
+  //   name: 'HBO Go',
+  //   slug: 'hbo-go',
+  //   message: 'penambahan pajak 11%',
+  // },
+  // {
+  //   name: 'Canva',
+  //   slug: 'canva',
+  //   message: 'Provider Canva memperbarui harga berlangganannya',
+  // },
+  // {
+  //   name: 'Nord VPN',
+  //   slug: 'nord-vpn',
+  //   message: 'Provider Nord VPN memperbarui harga berlangganannya',
+  // },
+  // {
+  //   name: 'Setapp',
+  //   slug: 'setapp',
+  //   message: 'perubahan kurs Rupiah terhadap Dollar',
+  // },
+  // {
+  //   name: 'Youtube',
+  //   slug: 'youtube',
+  //   message: 'perubahan pajak dari 10% menjadi 11%',
+  // },
+  // {
+  //   name: 'Google One',
+  //   slug: 'google-one',
+  //   message: 'perubahan pajak dari 10% menjadi 11%',
+  // },
   {
-    name: 'HBO Go',
-    slug: 'hbo-go',
+    name: 'Scribd',
+    slug: 'scribd',
     message: 'penambahan pajak 11%',
   },
   {
-    name: 'Canva',
-    slug: 'canva',
-    message: 'Provider Canva memperbarui harga berlangganannya',
+    name: 'Apple One',
+    slug: 'apple-one',
+    message: 'kenaikan biaya berlangganan seluruh provider Apple',
   },
   {
-    name: 'Nord VPN',
-    slug: 'nord-vpn',
-    message: 'Provider Nord VPN memperbarui harga berlangganannya',
+    name: 'Apple One Premier',
+    slug: 'apple-one-premier',
+    message: 'kenaikan biaya berlangganan seluruh provider Apple',
   },
   {
-    name: 'Setapp',
-    slug: 'setapp',
-    message: 'perubahan kurs Rupiah terhadap Dollar',
+    name: 'Apple Music',
+    slug: 'apple-music',
+    message: 'kenaikan biaya berlangganan seluruh provider Apple',
   },
   {
-    name: 'Youtube',
-    slug: 'youtube',
-    message: 'perubahan pajak dari 10% menjadi 11%',
-  },
-  {
-    name: 'Google One',
-    slug: 'google-one',
-    message: 'perubahan pajak dari 10% menjadi 11%',
+    name: 'Apple TV',
+    slug: 'apple-tv',
+    message: 'kenaikan biaya berlangganan seluruh provider Apple',
   },
 ];
