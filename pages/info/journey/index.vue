f3e22289db7455fa8c45ae5cb9bf7fc7569cb095
ibnu hazar <ibnuhazar101@gmail.com>
2022-11-20 20:07:29 +0700

    add journey page

---
+++ pages/info/journey/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Header />
+    <Journey />
+    <SeakunTeam />
+    <ComingProducts />
+    <Donation />
+  </div>
+</template>
+
+<script>
+import Header from './views/Header.vue';
+import Journey from './views/Journey.vue';
+import SeakunTeam from './views/SeakunTeam.vue';
+import ComingProducts from './views/ComingProducts.vue';
+import Donation from './views/Donation.vue';
+export default {
+  layout: 'new',
+  components: {
+    Header,
+    Journey,
+    SeakunTeam,
+    ComingProducts,
+    Donation,
+  },
+};
+</script>
+
+<style></style>
