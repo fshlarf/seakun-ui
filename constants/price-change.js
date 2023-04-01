220ff7c5fd4239d933920c44383994fe67cb1a07
ibnuhazar101 <ibnuhazar101@gmail.com>
2023-04-01 10:07:42 +0700

    remove info price change

--- constants/price-change.js
+++ constants/price-change.js
@@ -9,49 +9,49 @@ export const priceChangeList = [
   //   slug: 'canva',
   //   message: 'Provider Canva memperbarui harga berlangganannya',
   // },
-  {
-    name: 'Nord VPN',
-    slug: 'nord-vpn',
-    message: 'perubahan kurs Rupiah terhadap Dollar',
-  },
+  // {
+  //   name: 'Nord VPN',
+  //   slug: 'nord-vpn',
+  //   message: 'perubahan kurs Rupiah terhadap Dollar',
+  // },
   // {
   //   name: 'Setapp',
   //   slug: 'setapp',
   //   message: 'perubahan kurs Rupiah terhadap Dollar',
   // },
-  {
-    name: 'Youtube',
-    slug: 'youtube',
-    message: 'kenaikan harga provider youtube dari Rp89.000 menjadi Rp99.000',
-  },
+  // {
+  //   name: 'Youtube',
+  //   slug: 'youtube',
+  //   message: 'kenaikan harga provider youtube dari Rp89.000 menjadi Rp99.000',
+  // },
   // {
   //   name: 'Google One',
   //   slug: 'google-one',
   //   message: 'perubahan pajak dari 10% menjadi 11%',
   // },
-  {
-    name: 'Scribd',
-    slug: 'scribd',
-    message: 'penambahan pajak 11%',
-  },
-  {
-    name: 'Apple One',
-    slug: 'apple-one',
-    message: 'kenaikan biaya berlangganan seluruh provider Apple',
-  },
-  {
-    name: 'Apple One Premier',
-    slug: 'apple-one-premier',
-    message: 'kenaikan biaya berlangganan seluruh provider Apple',
-  },
-  {
-    name: 'Apple Music',
-    slug: 'apple-music',
-    message: 'kenaikan biaya berlangganan seluruh provider Apple',
-  },
-  {
-    name: 'Apple TV',
-    slug: 'apple-tv',
-    message: 'kenaikan biaya berlangganan seluruh provider Apple',
-  },
+  // {
+  //   name: 'Scribd',
+  //   slug: 'scribd',
+  //   message: 'penambahan pajak 11%',
+  // },
+  // {
+  //   name: 'Apple One',
+  //   slug: 'apple-one',
+  //   message: 'kenaikan biaya berlangganan seluruh provider Apple',
+  // },
+  // {
+  //   name: 'Apple One Premier',
+  //   slug: 'apple-one-premier',
+  //   message: 'kenaikan biaya berlangganan seluruh provider Apple',
+  // },
+  // {
+  //   name: 'Apple Music',
+  //   slug: 'apple-music',
+  //   message: 'kenaikan biaya berlangganan seluruh provider Apple',
+  // },
+  // {
+  //   name: 'Apple TV',
+  //   slug: 'apple-tv',
+  //   message: 'kenaikan biaya berlangganan seluruh provider Apple',
+  // },
 ];
