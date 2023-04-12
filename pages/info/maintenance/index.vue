4e9f5fc7710820821b2a26697aab421c009d02a8
ibnuhazar101 <ibnuhazar101@gmail.com>
2023-04-12 09:56:31 +0700

    turn on maintenance

--- pages/info/maintenance/index.vue
+++ pages/info/maintenance/index.vue
@@ -17,8 +17,8 @@
       </p>
       <br />
       <p class="md:text-[20px] text-[#2d2d2d] opacity-80">
-        Kembali aktif lagi Jum'at, 18 November 2022 pukul 09.00 WIB ya. See you!
-        <!-- Kembali aktif lagi malam ini pukul 23.59 WIB ya. See you! -->
+        <!-- Kembali aktif lagi Jum'at, 18 November 2022 pukul 09.00 WIB ya. See you! -->
+        Kembali aktif lagi siang ini pukul 12.00 WIB ya. See you!
       </p>
       <a
         class="w-full"
@@ -42,9 +42,9 @@ export default {
   components: {
     Button,
   },
-  beforeMount() {
-    this.$router.push('/');
-  },
+  // beforeMount() {
+  //   this.$router.push('/');
+  // },
 };
 </script>
 
