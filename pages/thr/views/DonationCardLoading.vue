ace203db94daac3222359392d0a82c58c82992f2
ibnu hazar <69763294+ibnuhazar101@users.noreply.github.com>
2023-04-20 13:27:27 +0700

    [M][LANDING-PAGE][IMP] integration donation api (#313)

---
+++ pages/thr/views/DonationCardLoading.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tn:w-[300px] xl:w-full rounded-xl bg-white p-[20px]">
+    <div class="shimmer w-1/2 h-4"></div>
+    <div class="flex space-x-2 items-center text-primary">
+      <div class="shimmer tn:ml-2 w-5 h-5 !rounded-full"></div>
+      <div class="shimmer w-3/5 h-3"></div>
+    </div>
+    <div class="shimmer h-6 w-3/4 tn:mt-3"></div>
+    <div class="tn:mt-2">
+      <img src="/images/icons/atoms/quote.svg" alt="quote icon" />
+      <p class="shimmer w-full tn:mt-3 h-3"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/thr/views/ModalDetailDonatur.vue
@@ -0,0 +1,70 @@
+<template>
+  <Modal :is-show="showModal" @onClose="onClose" size="xl:w-1/3">
+    <div class="bg-white p-[10px]">
+      <p>{{ donaturDetail.name }}</p>
+      <div v-if="donaturDetail.socialMedia && donaturDetail.accountName">
+        <a
+          v-if="!donaturDetail.accountName.includes('*')"
+          :href="`${
+            donaturDetail.socialMedia === 'twitter'
+              ? 'https://twitter.com/'
+              : 'https://instagram.com/'
+          }${donaturDetail.accountName}`"
+          target="_blank"
+          class="flex space-x-1 items-center text-primary"
+        >
+          <i :class="`fa-brands fa-${donaturDetail.socialMedia}`"></i>
+          <p class="text-[14px]">{{ donaturDetail.accountName }}</p>
+        </a>
+        <div v-else class="flex space-x-1 items-center text-primary">
+          <i :class="`fa-brands fa-${donaturDetail.socialMedia}`"></i>
+          <p class="text-[14px]">{{ donaturDetail.accountName }}</p>
+        </div>
+      </div>
+      <div v-else class="flex space-x-2 items-center text-[#D9D9D9]">
+        <img
+          src="/images/icons/atoms/link-disabled.svg"
+          alt="no social media"
+        />
+        <p class="text-[14px]">tidak tercantum</p>
+      </div>
+      <p class="text-[20px] font-bold tn:mt-2 text-secondary">
+        {{ currencyFormat(donaturDetail.nominal) }}
+      </p>
+      <div class="tn:mt-5">
+        <img src="/images/icons/atoms/quote.svg" alt="quote icon" />
+        <p class="text-[14px] leading-[20px] tn:mt-2">
+          {{ donaturDetail.message }}
+        </p>
+      </div>
+    </div>
+  </Modal>
+</template>
+
+<script>
+import Modal from '~/components/atoms/Modal.vue';
+import { currencyFormat } from '../../../helpers';
+export default {
+  components: {
+    Modal,
+  },
+  props: {
+    showModal: {
+      type: Boolean,
+      default: false,
+    },
+    donaturDetail: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  methods: {
+    onClose() {
+      this.$emit('onClose');
+    },
+    currencyFormat,
+  },
+};
+</script>
+
+<style></style>
