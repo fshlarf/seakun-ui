87e741bca08b55fab41a90f1a419fa6beba33c2b
Fatma Wani <ranafatma16@gmail.com>
2023-05-05 17:49:02 +0700

    Seatap landing page

---
+++ pages/seatap/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navbar />
+    <Header />
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/mollecules/NavbarBlank.vue';
+import Header from './views/Header';
+
+export default {
+  components: {
+    Navbar,
+    Header,
+  },
+};
+</script>
