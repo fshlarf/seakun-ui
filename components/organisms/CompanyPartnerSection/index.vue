b7d0fe7894977b2bb8902adc189aed6b8b1c6440
FatmaWaniSilitonga <77450027+FatmaWaniSilitonga@users.noreply.github.com>
2023-06-20 16:07:51 +0700

    [S] [LANDING-PAGE] [IMP] edit company partner (#332)

--- components/organisms/CompanyPartnerSection/index.vue
+++ components/organisms/CompanyPartnerSection/index.vue
@@ -4,20 +4,20 @@
       <h1
         class="font-bold tn:text-[20px] md:text-[30px] xl:text-[36px] tn:mb-4 lg:mb-8"
       >
-        Partner
+        Partner Seakun
       </h1>
     </div>
     <div
-      class="w-full h-full grid tn:grid-cols-2 md:grid-cols-4 tn:gap-3 lg:gap-6 place-items-stretch items-stretch"
+      class="w-full h-full grid tn:grid-cols-2 md:grid-cols-3 tn:gap-3 lg:gap-x-[15px] lg:gap-y-[40px] place-items-stretch items-stretch"
     >
       <div
         v-for="(partner, id) in partners"
         :key="id"
-        class="tn:px-6 tn:py-2 flex justify-center items-center"
+        class="flex justify-center items-center"
       >
         <img
           class="w-full"
-          :src="`/images/partners/${partner}.png`"
+          :src="`/images/partners/new/${partner}.png`"
           alt="partner logo"
         />
       </div>
@@ -30,7 +30,14 @@ import UserTypeCard from '/components/mollecules/UserTypeCard.vue';
 export default {
   data() {
     return {
-      partners: ['xendit', 'pejer', 'axdif', 'indigo-space'],
+      partners: [
+        'xendit',
+        'flip',
+        'transfez',
+        'support-seller',
+        'axdif',
+        'kitabisa',
+      ],
     };
   },
   components: {
