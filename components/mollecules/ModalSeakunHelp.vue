81e41846edffa40832c24a10d78b71f58fff4556
ibnuhazar101 <ibnuhazar101@gmail.com>
2023-08-02 08:11:46 +0700

    [M][LANDING-PAGE][IMP] update modal seakun help

--- components/mollecules/ModalSeakunHelp.vue
+++ components/mollecules/ModalSeakunHelp.vue
@@ -7,7 +7,9 @@
         alt="seakun help"
       />
 
-      <div class="tn:hidden md:block md:mt-[20px] space-y-[12px]">
+      <div
+        class="tn:mt-[12px] md:mt-[20px] tn:space-y-[10px] md:space-y-[12px]"
+      >
         <div
           class="rounded-[8px] border-[#8DCABE] border-[2px] overflow-hidden w-[306px] flex items-center gap-[12px] gradient"
           role="button"
@@ -43,54 +45,6 @@
               dll
             </p>
           </div>
-        </div>
-      </div>
-
-      <div class="md:hidden mt-[20px] space-y-[10px]">
-        <div
-          class="w-[232px] border-[2px] border-[#8DCABE] rounded-[8px] h-[44px] flex items-center justify-center gap-[8px] mx-auto"
-          :class="`${isHovered ? 'bg-[#4BAC99]' : 'gradient'}`"
-          role="button"
-          @click="toSeakunHelp"
-        >
-          <IconQuestionMark
-            :color="`${isHovered ? '#ffffff' : '#4BAC99'}`"
-            @mouseover="isHovered = !isHovered"
-          />
-          <p
-            class="text-[14px]"
-            :class="`${isHovered ? 'text-white' : 'text-[#4BAC99]'}`"
-          >
-            Seakun Help
-          </p>
-        </div>
-
-        <div
-          class="w-[232px] border-[2px] border-[#8DCABE] rounded-[8px] h-[44px] flex items-center justify-center gap-[8px] mx-auto"
-          :class="`${isHovered ? 'bg-[#4BAC99]' : 'gradient'}`"
-          role="button"
-          @click="toAppointment"
-        >
-          <IconAppointment
-            :color="`${isHovered ? '#ffffff' : '#4BAC99'}`"
-            @mouseover="isHovered = !isHovered"
-          />
-          <p
-            class="text-[14px]"
-            :class="`${isHovered ? 'text-white' : 'text-[#4BAC99]'}`"
-          >
-            Janjian Login
-          </p>
-        </div>
-
-        <div
-          class="flex items-start justify-center space-x-[4px] w-[232px] mx-auto"
-        >
-          <IconInfo class="mt-[2px]" color="#F6B205" />
-          <p class="text-[#F6B205] !text-[11px] w-[214px] font-medium">
-            Janjian Login Disney, Netflix (device TV), Prime Video, Zoom, Apple,
-            Medium.
-          </p>
         </div>
       </div>
     </div>
