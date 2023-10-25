f204db83297884cb677389066eaefd2cd1d6d319
ibnuhazar101 <ibnuhazar101@gmail.com>
2023-10-25 12:16:10 +0700

    update payment page

--- pages/payment/views/OrderDetail.vue
+++ pages/payment/views/OrderDetail.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="mt-[24px] md:mt-[28px]">
     <div
-      class="flex-1 flex-column p-3 rounded border-l-4 border-green-seakun bg-white"
+      class="flex-1 flex-column p-3 rounded border-l-4 border-green-seakun bg-[#E9FCF8]/30"
     >
-      <p class="text-lg font-bold">Detail Pemesan</p>
+      <p class="text-sm md:text-[20px] font-bold">Detail Pelanggan</p>
       <div class="my-3 border-b border-[#E5E5E5]" />
       <div v-if="isLoading">
         <p class="shimmer w-5/12"></p>
@@ -11,9 +11,22 @@
         <p class="shimmer w-8/12"></p>
       </div>
       <div v-else>
-        <p class="text-base font-bold">{{ orderDetail.name }}</p>
-        <p class="my-2 text-base font-normal">{{ orderDetail.phone }}</p>
-        <p class="text-base font-normal">{{ orderDetail.email }}</p>
+        <div class="flex items-center gap-2">
+          <img src="/images/icons/atoms/profile-green.svg" alt="nama" />
+          <p class="text-sm md:text-base font-bold">{{ orderDetail.name }}</p>
+        </div>
+        <div class="flex items-center gap-2">
+          <img src="/images/icons/atoms/phone-green.svg" alt="nama" />
+          <p class="my-2 text-sm md:text-base font-normal">
+            {{ orderDetail.phone }}
+          </p>
+        </div>
+        <div class="flex items-center gap-2">
+          <img src="/images/icons/atoms/email-green.svg" alt="nama" />
+          <p class="text-sm md:text-base font-normal">
+            {{ orderDetail.email }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
---
+++ pages/payment/views/PaymentMethod.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="w-full mt-[24px] md:mt-[28px]">
+    <h2 class="text-sm md:text-[20px] font-bold">Pilih Metode Pembayaran</h2>
+    <div class="mt-2 md:mt-4 grid grid-cols-5 gap-[8px] md:gap-[16px]">
+      <div
+        role="button"
+        @click="$emit('selectMethod', method)"
+        v-for="(method, id) in methodList"
+        :key="id"
+        class="rounded-[5px] w-full h-[44px] md:h-[80px] flex justify-center items-center border p-2 md:p-4"
+        :class="`${
+          method.code == selectedMethod.code
+            ? 'border-primary border-2 bg-[#E9FCF8]/30'
+            : 'border-slate-100'
+        }`"
+      >
+        <img
+          :src="`/images/payment/${method.slug}.png`"
+          alt="metode pembayaran"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    methodList: {
+      type: Array,
+      default: [],
+    },
+    selectedMethod: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style></style>
