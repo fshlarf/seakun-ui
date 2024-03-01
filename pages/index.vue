4c7d29fa68af10cf5a78cd212400f2939f5aa565
ibnuhazar101 <ibnuhazar101@gmail.com>
2024-03-01 11:03:32 +0700

    [M][LANDING-PAGE][IMP] turn on maintenance page

--- pages/index.vue
+++ pages/index.vue
@@ -85,9 +85,9 @@ export default {
       groupList: 'getGroups',
     }),
   },
-  // beforeMount() {
-  //   this.$router.push('/info/maintenance');
-  // },
+  beforeMount() {
+    this.$router.push('/info/maintenance');
+  },
   mounted() {
     this.checkReferralCode();
     const userGroup = document.getElementById('pengguna');
