e7eea9ac8aae79a315ebc7622d216096ac7278a9
ibnuhazar101 <ibnuhazar101@gmail.com>
2024-04-16 11:16:08 +0700

    [M][LANDING-PAGE][DEBUG] add dev payment page

---
+++ pages/payment-dev/views/ModalDuration.vue
@@ -0,0 +1,101 @@
+<template>
+  <Modal :is-show="showModal" @onClose="onClose" size="xl:w-2/6">
+    <template v-slot:header>
+      <div class="font-bold text-lg">Ubah Durasi Berlangganan</div>
+    </template>
+    <div class="flex items-center space-x-2 mb-8">
+      <img
+        :src="`/images/product/brand/${orderData.packageName}.png`"
+        alt="provider brand"
+        class="w-20 h-auto"
+      />
+      <p class="text-base font-bold ml-2">{{ orderData.orderNumber }}</p>
+    </div>
+    <div v-if="isLoading">
+      <div
+        class="border rounded-xl py-3 px-6 mb-2 cursor-pointer"
+        v-for="(item, index) in dataLoading"
+        :key="index"
+      >
+        <p class="shimmer w-full"></p>
+      </div>
+    </div>
+    <div v-else>
+      <div
+        v-for="(item, index) in listDuration"
+        :key="index"
+        v-bind:style="[
+          item.uid === orderData.variantUid
+            ? { background: '#EBFCFF', border: '1.5px solid #8DCABE' }
+            : { border: '1.5px solid #ededed' },
+        ]"
+        class="border-2 rounded-xl py-3 px-6 mb-2 cursor-pointer"
+        @click="$emit('pickDuration', item)"
+      >
+        <p v-if="item.duration === 12" class="text-lg font-normal">
+          1 Tahun ({{ currencyFormat(item.grandTotal) }})
+        </p>
+        <p v-else class="text-lg font-normal">
+          {{ item.duration }} Bulan ({{ currencyFormat(item.grandTotal) }})
+        </p>
+      </div>
+    </div>
+  </Modal>
+</template>
+<script>
+import Modal from '~/components/atoms/Modal';
+import { currencyFormat } from '~/helpers/word-transformation.js';
+
+export default {
+  name: 'ModalDuration',
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      currencyFormat,
+      dataLoading: [0, 1, 2],
+      provider: ['microsoft', 'microsoft-365', 'microsoft365', 'canva'], // special case for provider with different price for 1 year subscription
+    };
+  },
+  props: {
+    showModal: {
+      type: Boolean,
+      default: false,
+    },
+    durationData: {
+      type: Array,
+      default: [],
+    },
+    orderData: Object,
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    listDuration() {
+      if (this.orderData) {
+        if (this.provider.includes(this.orderData.packageName)) {
+          if ([1, 3, 6].includes(this.orderData.duration)) {
+            return this.durationData.filter((item) => {
+              return item.duration !== 12;
+            });
+          } else {
+            return this.durationData.filter((item) => {
+              return item.duration === 12;
+            });
+          }
+        } else {
+          return this.durationData;
+        }
+      }
+    },
+  },
+  methods: {
+    onClose() {
+      this.$emit('onClose');
+    },
+  },
+};
+</script>
---
+++ pages/payment-dev/views/OrderDetail.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="mt-[24px] md:mt-[28px]">
+    <div
+      class="flex-1 flex-col p-3 rounded border-l-4 border-green-seakun bg-[#E9FCF8]/30"
+    >
+      <p class="text-sm md:text-[20px] font-bold">Detail Pelanggan</p>
+      <div class="my-3 border-b border-[#E5E5E5]" />
+      <div v-if="isLoading">
+        <p class="shimmer w-5/12"></p>
+        <p class="shimmer w-9/12 my-2"></p>
+        <p class="shimmer w-8/12"></p>
+      </div>
+      <div v-else>
+        <div class="flex items-center gap-2">
+          <img src="/images/icons/atoms/profile-green.svg" alt="nama" />
+          <p class="text-sm md:text-base font-bold">{{ orderDetail.name }}</p>
+        </div>
+        <div class="flex items-center gap-2">
+          <img src="/images/icons/atoms/phone-green.svg" alt="nama" />
+          <p class="my-2 text-sm md:text-base font-normal">
+            {{ orderDetail.phone }}
+          </p>
+        </div>
+        <div class="flex items-center gap-2">
+          <img src="/images/icons/atoms/email-green.svg" alt="nama" />
+          <p class="text-sm md:text-base font-normal">
+            {{ orderDetail.email }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'OrderDetail',
+  props: {
+    orderDetail: Object,
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
