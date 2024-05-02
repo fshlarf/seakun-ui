336f5efc63c433cfce97c19f177a6c4fc3ec9b5d
ibnu hazar <69763294+ibnuhazar101@users.noreply.github.com>
2024-05-02 13:43:34 +0700

    [S][LANDING-PAGE][DEV] blog landing page (#410)

---
+++ components/mollecules/PaginationButtonBlog.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    @click="$emit('handleClick')"
+    class="flex h-[32px] w-[32px] items-center justify-center rounded-[8px] border border-[#F1F1F1] bg-white"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    disable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
