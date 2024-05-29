acbaf6e7d8ebd2f2f5db1d0251a072f097250008
zuhriyal <92376249+zuhriyal@users.noreply.github.com>
2024-05-29 22:39:14 +0700

    [M][LANDING-PAGE][IMP] Canva migration (#433)

---
+++ pages/info/canva-migration/components/CardForPrice.vue
@@ -0,0 +1,64 @@
+<template>
+  <div
+    class="w-full sm:w-[400px] md:w-[390px] bg-white p-4 md:p-5 rounded-lg md:rounded-[10px]"
+    style="box-shadow: 0 2px 30px rgba(0, 0, 0, 0.1)"
+  >
+    <header
+      class="border rounded-md"
+      :class="[
+        cardType == 'old'
+          ? 'text-[#9638FF] bg-[#F5F1FF]  border-[#EEE7FF]'
+          : 'text-[#028981] bg-[#E0F8FC]  border-[#D4EDF1]',
+      ]"
+    >
+      <p
+        class="font-bold text-sm sm:text-base py-[9px] sm:py-[11px] text-center"
+      >
+        {{ cardType == 'old' ? 'Harga Lama Seakun' : 'Harga Baru Seakun' }}
+      </p>
+    </header>
+    <div class="text-xs lg:text-sm mt-2 space-y-2 text-gray-secondary">
+      <section class="flex justify-between items-center">
+        <p>Nama Paket</p>
+        <p>{{ information.name }}</p>
+      </section>
+      <section class="flex justify-between items-center">
+        <p>Harga Provider</p>
+        <p>{{ information.price }}</p>
+      </section>
+      <section class="flex justify-between items-center">
+        <p>Jumlah member Per grup</p>
+        {{ information.member }}
+      </section>
+      <section class="flex justify-between items-center">
+        <p>Harga Patungan</p>
+        <p>{{ information.jointVenturePrice }}</p>
+      </section>
+      <section class="flex justify-between items-center">
+        <p>Biaya Admin Seakun</p>
+        <p>{{ information.adminFee }}</p>
+      </section>
+      <section class="flex justify-between items-center font-bold">
+        <p>Harga Paket Bulanan</p>
+        <p class="text-primary">{{ information.total }}</p>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cardType: {
+      typeof: String,
+      default: 'old',
+    },
+    information: {
+      typeof: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/info/canva-migration/components/PriceChanges.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="w-full md:bg-[#F5F1FF] mt-8 md:mt-10">
+    <div class="container md:pt-9 md:pb-11 lg:pt-12 lg:pb-14">
+      <header>
+        <p class="font-bold text-base md:text-lg lg:text-[20px] text-center">
+          Perubahan Harga di Seakun
+        </p>
+        <div class="flex justify-center gap-3 mt-3 sm:mt-6">
+          <Button
+            class="!text-xs sm:!text-sm w-[113px] h-9 !rounded-md border"
+            :class="[
+              isMonthly
+                ? 'bg-[#9638FF] border-[#9638FF] text-white'
+                : 'bg-[#F0EAFF] text-[#2D2D2D]',
+            ]"
+            @click="isMonthly = true"
+            >Bulanan</Button
+          >
+          <Button
+            class="!text-xs sm:!text-sm w-[113px] h-9 !rounded-md border"
+            :class="[
+              !isMonthly
+                ? 'bg-[#9638FF] border-[#9638FF] text-white'
+                : 'bg-[#F0EAFF] border-[#E6E0F4] text-[#2D2D2D]',
+            ]"
+            @click="isMonthly = false"
+            >Tahunan</Button
+          >
+        </div>
+      </header>
+      <main
+        class="flex flex-col md:flex-row mt-5 md:mt-9 gap-4 sm:gap-6 lg:gap-8 sm:justify-center items-center"
+      >
+        <CardForPrice :information="oldDataPrice" />
+        <img
+          :src="arrowSrc"
+          alt="arrow"
+          class="w-10 h-[50px] sm:w-[65px] sm:h-[60px] lg:w-[100px] lg:h-[67px]"
+        />
+        <CardForPrice cardType="new" :information="newDataPrice" />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from '../../../../components/atoms/Button.vue';
+import CardForPrice from './CardForPrice.vue';
+export default {
+  components: {
+    Button,
+    CardForPrice,
+  },
+  data() {
+    return {
+      isMonthly: true,
+      arrowSrc: '',
+      oldMonthlyPrice: {
+        name: 'Canva (Bulanan)',
+        price: 'Rp190.000',
+        member: 4,
+        jointVenturePrice: 'Rp190.000 ÷ 4 = Rp47.500',
+        adminFee: 'Rp6.500',
+        total: 'Rp54.000',
+      },
+      newMonthlyPrice: {
+        name: 'Canva (Bulanan)',
+        price: 'Rp365.000',
+        member: 5,
+        jointVenturePrice: 'Rp365.000 ÷ 5 = Rp73.000',
+        adminFee: 'Rp6.500',
+        total: 'Rp79.500',
+      },
+      oldAnnualPrice: {
+        name: 'Canva (Tahunan)',
+        price: 'Rp1.900.000',
+        member: 4,
+        jointVenturePrice: 'Rp1.900.000 ÷ 4 = Rp475.000',
+        adminFee: 'Rp70.000',
+        total: 'Rp545.000',
+      },
+      newAnnualPrice: {
+        name: 'Canva (Tahunan)',
+        price: 'Rp3.650.000',
+        member: 5,
+        jointVenturePrice: 'Rp3.650.000 ÷ 5 = Rp730.000',
+        adminFee: 'Rp78.000',
+        total: 'Rp808.000',
+      },
+    };
+  },
+  computed: {
+    oldDataPrice() {
+      if (this.isMonthly) {
+        return this.oldMonthlyPrice;
+      } else return this.oldAnnualPrice;
+    },
+    newDataPrice() {
+      if (this.isMonthly) {
+        return this.newMonthlyPrice;
+      } else return this.newAnnualPrice;
+    },
+  },
+  mounted() {
+    this.handleArrowSrc();
+    window.addEventListener('resize', this.handleArrowSrc);
+  },
+  methods: {
+    handleArrowSrc() {
+      if (window.innerWidth < 768) {
+        this.arrowSrc = '/images/canva-migration/arrow-mobile.webp';
+      } else this.arrowSrc = '/images/canva-migration/arrow.webp';
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
