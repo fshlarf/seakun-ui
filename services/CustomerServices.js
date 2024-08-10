643ba45052f441058a87afb0f179197aa1f35d1f
ibnuhazar101 <ibnuhazar101@gmail.com>
2024-08-10 13:07:37 +0700

    integration api sefoursary

--- services/CustomerServices.js
+++ services/CustomerServices.js
@@ -15,6 +15,49 @@ class CustomerService {
   updateCustomer(params) {
     return this.serviceApiAuth.patch('/customer', { ...params });
   }
+
+  // get from all sheet
+  // req param query: sheet (string)
+  getSefoursaryData(params) {
+    return this.serviceApiAuth.get('/customer/sefoursary', { params });
+  }
+
+  // api flow:
+  // check available prize in sheet HADIAH
+  // draw lottery
+  // update sheet HADIAH
+  // save to sheet UNDIAN
+  // save to sheet WIN
+  // open next level in sheet USER
+  // req body: level (number)
+  drawRandomGiftSefoursary(params) {
+    return this.serviceApiAuth.post('/customer/sefoursary', { ...params });
+  }
+
+  // open next level in sheet USER
+  // req body: level (number) (current level)
+  openNextLevelSefoursary(params) {
+    return this.serviceApiAuth.post('/customer/sefoursary/next-level', {
+      ...params,
+    });
+  }
+
+  // for page approval (done)
+  generateUniqueCodeSefoursary(params) {
+    return this.serviceApiAuth.post('/customer/sefoursary/unique-code', {
+      ...params,
+    });
+  }
+
+  // for page thankyou (done)
+  generateUniqueCodeThankyouSefoursary(params) {
+    return this.serviceApiAuth.post(
+      '/customer/sefoursary/unique-code/thankyou-page',
+      {
+        ...params,
+      }
+    );
+  }
 }
 
 export default CustomerService;
